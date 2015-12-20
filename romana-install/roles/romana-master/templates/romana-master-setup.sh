8d6c0ff13163c210450fff889ca415ce834e0fdc
Caleb Gilmour <cgilmour@gmail.com>
2015-12-21 12:19:37 +1300

    Overwrite etc/hosts with romana predefined IPs

--- romana-install/roles/romana-master/templates/romana-master-setup.sh
+++ romana-install/roles/romana-master/templates/romana-master-setup.sh
@@ -19,3 +19,7 @@ curl -X POST -H "Content-Type: application/json" --data '{"name": "ip-192-168-0-
 curl -X POST -H "Content-Type: application/json" --data '{"name": "ip-192-168-0-12", "ip": "192.168.0.12", "romana_ip": "10.2.0.0/16", "agent_port": 9604 }' http://{{ devstack_controller }}:9603/hosts
 curl -X POST -H "Content-Type: application/json" --data '{"name": "ip-192-168-0-13", "ip": "192.168.0.13", "romana_ip": "10.3.0.0/16", "agent_port": 9604 }' http://{{ devstack_controller }}:9603/hosts
 curl -X POST -H "Content-Type: application/json" --data '{"name": "ip-192-168-0-14", "ip": "192.168.0.14", "romana_ip": "10.4.0.0/16", "agent_port": 9604 }' http://{{ devstack_controller }}:9603/hosts
+
+# TODO: Remove this when ML2 driver appropriately looks up agent IP:Port
+# Workaround for ML2 driver: Add agent hostnames into /etc/hosts
+cp /var/tmp/romana-hosts /etc/hosts
