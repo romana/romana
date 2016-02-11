853b0608f186a9d0faafae746acad59caf60f4d3
Juergen Brendel <juergen@brendel.com>
2016-02-12 09:44:31 +1300

    Changed iptables code.

--- kubernetes/k8s-listener.py
+++ kubernetes/k8s-listener.py
@@ -2,53 +2,21 @@ import httplib
 import requests
 import sys
 import simplejson
-from subprocess import call
-
-def hook(r, *args, **kwargs):
-    print r
+import subprocess
 
 url = 'http://192.168.0.10:8080/apis/romana.io/demo/v1/namespaces/default/networkpolicys/?watch=true'
 tenant_url = 'http://192.168.0.10:9602/tenants'
 
-def parse_rule_specs(obj):
-    rule = {}
-    rule["src_tenant"] = obj["object"]["metadata"]["labels"]["owner"]
-    rule["dst_tenant"] = rule["src_tenant"]
-    rule["dst_segment"] = obj["object"]["spec"]["podSelector"]["tier"]
-    rule["src_segment"] = obj["object"]["spec"]["allowIncoming"]["from"][0]["pods"]["tier"]
-    rule["port"] = obj["object"]["spec"]["allowIncoming"]["toPorts"][0]["port"]
-    rule["protocol"] = obj["object"]["spec"]["allowIncoming"]["toPorts"][0]["protocol"]
-    return rule
 
-def get_tenants():
-    r = requests.get(tenant_url)
-    return simplejson.loads(r.content)
+def _make_rule(chain_name, text):
+    """
+    Returns "-A <chain_name> <text>"
 
-def get_tenant_id_by_name(name, tenants):
-    for tenant in tenants:
-        if tenant['Name'] == name:
-            return tenant['Id']
+    """
+    return "-A %s %s" % (chain_name, text)
 
-def get_segments(tenant_id):
-    r = requests.get(tenant_url + '/' + str(tenant_id) + '/segments')
-    return simplejson.loads(r.content)
-
-def get_segment_id_by_name(name, segments):
-    for segment in segments:
-        if segment['Name'] == name:
-            return segment['Seq']
-
-addr_scheme = {
-    "network_width" : 8,
-    "host_width" : 8,
-    "tenant_width" : 4,
-    "segment_width" : 4,
-    "endpoint_width" : 8,
-
-    "network_value" : 10,
-}
-
-def _make_u32_match(from_tenant, from_segment, to_tenant, to_segment):
+def _make_u32_match(addr_scheme,
+                    from_tenant, from_segment, to_tenant, to_segment):
     """
     Creates the obscure u32 match string with bitmasks and all that's needed.
 
@@ -81,110 +49,313 @@ def _make_u32_match(from_tenant, from_segment, to_tenant, to_segment):
     return "0xc&0x%(mask)x=0x%(src)x&&0x10&0x%(mask)x=0x%(dst)x" % \
         { "mask" : mask, "src" : src, "dst" : dst }
 
-def makeU32rule(tenant_id, segment_id):
-    net = 10
-    host_bits = 8
-    tenant_bits = 4
-    segment_bits = 4
-    endpoint_bits = 8
-    return "stub"
 
-def make_forward_name(tenant_id, segment_id):
-    name = 'ROMANA-T%sS%s-FORWARD' % (tenant_id, segment_id)
-    return name
+def make_rules(addr_scheme, policy_def):
+    """
+    Return dictionary with rules that should be pre-pended to given chains.
 
-def make_base_rules(firewall_policy_name, src_mask, dst_mask):
-    base_rules=[]
-    base_rules.append('-A %s -m u32 --u32 %s -j %s-OUT' % (firewall_policy_name, dst_mask, firewall_policy_name))
-    base_rules.append('-A %s -m u32 --u32 %s -j %s-IN' % (firewall_policy_name, src_mask, firewall_policy_name))
-    base_rules.append('-A %s -j RETURN' % firewall_policy_name)
-    return base_rules
+    The chain names are the keys to the dict, the values are lists of rules.
 
-def make_out_rules(firewall_policy_name, port):
-    out_rules=[]
-    out_rules.append('-A %s-OUT -p tcp --dport %s --tcp-flags SYN SYN -j ACCEPT' % (firewall_policy_name, port))
-    out_rules.append('-A %s-OUT -m state --state ESTABLISHED -j ACCEPT' % firewall_policy_name)
-    out_rules.append('-A %s-OUT -j RETURN' % firewall_policy_name)
-    return out_rules
+    """
+    rules = {}
+    tenant         = policy_def['owner_tenant_id']
+    target_segment = policy_def['target_segment_id']
+    from_segment   = policy_def['allowFrom']['segment_id']
 
-def make_in_rules(firewall_policy_name, port):
-    in_rules=[]
-    in_rules.append('-A %s-IN -p tcp --sport %s --tcp-flags SYN SYN -j ACCEPT' % (firewall_policy_name, port))
-    in_rules.append('-A %s-IN -m state --state ESTABLISHED -j ACCEPT' % firewall_policy_name)
-    in_rules.append('-A %s-IN -j RETURN' % firewall_policy_name)
-    return in_rules
+    # The name for the new policy's chain(s). Need to include the tenant ID to
+    # avoid name conflicts.
+    policy_chain_name = "ROMANA-T%dP-%s_" % \
+        (policy_def['owner_tenant_id'], policy_def['policy_name'])
 
-def make_policy_name(policy_name):
-    return 'ROMANA-P-%s' % policy_name
+    # This policy needs to be processed in the forward chain of both interfaces
+    # that are involved.
+    target_segment_forward_chain = "pani-T%sS%s-FORWARD" % \
+        (tenant, target_segment)
+    from_segment_forward_chain = "pani-T%sS%s-FORWARD" % \
+        (tenant, from_segment)
 
-def install_chains(firewall_policy_name):
-    base_policy = "iptables -N %s" % firewall_policy_name
-    in_policy  = "iptables -N %s-IN" % firewall_policy_name
-    out_policy = "iptables -N %s-OUT" % firewall_policy_name
-    print base_policy.split()
-    print in_policy.split()
-    print out_policy.split()
-    call(base_policy.split())
-    call(in_policy.split())
-    call(out_policy.split())
+    rules[target_segment_forward_chain] = [
+        _make_rule(target_segment_forward_chain, "-j %s" % policy_chain_name)
+    ]
+    rules[from_segment_forward_chain] = [
+        _make_rule(from_segment_forward_chain, "-j %s" % policy_chain_name)
+    ]
 
-def install_rules(rules):
-    for rule in rules:
-        full_rule = "iptables %s" % rule
-        print full_rule.split()
-        call(full_rule.split())
+    # Assemble the rules for the top-level policy chain. These rules look at
+    # the IP addresses (source and dest) and figure out whether this is
+    # incoming our outgoing traffic.
+    u32_in_match  = _make_u32_match(addr_scheme, tenant, from_segment,
+                                    tenant, target_segment)
+    u32_out_match = _make_u32_match(addr_scheme, tenant, target_segment,
+                                    tenant, from_segment)
 
-def install_first_jump(forward_chain, firewall_policy_name):
-    cmd = "iptables -I  %s 1 -j %s" % (forward_chain.replace("ROMANA","pani"), firewall_policy_name)
-    print cmd
-    call(cmd.split())
+    in_chain_name  = policy_chain_name[:-1] + "-IN_"
+    out_chain_name = policy_chain_name[:-1] + "-OUT_"
+
+    rules[policy_chain_name] = [
+        _make_rule(policy_chain_name, '-m u32 --u32 "%s" -j %s' %
+                                           (u32_in_match, in_chain_name)),
+        _make_rule(policy_chain_name, '-m u32 --u32 "%s" -j %s' %
+                                           (u32_out_match, out_chain_name)),
+        _make_rule(policy_chain_name, '-j RETURN')
+    ]
+
+    # Assemble the rules for the port/protocol portion of the policy.
+    # Note! We currently only support TCP with a given port. No ICMP and no
+    # UDP!
+
+    rules[in_chain_name] = [
+        _make_rule(in_chain_name,
+                   '-p tcp --dport %d --tcp-flags SYN SYN -j ACCEPT' %
+                   policy_def['allowFrom']['port']),
+        _make_rule(in_chain_name,
+                   '-m state --state ESTABLISHED -j ACCEPT'),
+        _make_rule(in_chain_name, '-j RETURN')
+    ]
+
+    rules[out_chain_name] = [
+        _make_rule(out_chain_name,
+                   '-p tcp --sport %d --tcp-flags SYN,ACK SYN,ACK -j ACCEPT' %
+                   policy_def['allowFrom']['port']),
+        _make_rule(out_chain_name,
+                   '-m state --state ESTABLISHED -j ACCEPT'),
+        _make_rule(out_chain_name, '-j RETURN')
+    ]
+
+    return rules
+
+def get_current_iptables():
+    """
+    Return the current iptables.
+
+    """
+    rules = subprocess.check_output(["iptables-save"]).split("\n")
+    return rules
+
+def delete_all_rules_for_policy(iptables_rules, policy_name, tenant_id):
+    """
+    Specify the policy name, such as 'foo'. This will delete all the rules that
+    refer to anything related to this rule, such as 'ROMANA-P-foo_',
+    'ROMANA-P-foo-IN_', etc.
+
+    """
+    full_names = [ 'ROMANA-T%dP-%s%s_' % (tenant_id, policy_name, p)
+                        for p in [ "", "-IN", "-OUT" ] ]
+
+    # Only transcribe those lines that don't mention any of the chains
+    # related to the policy.
+    clean_rules = [ r for r in iptables_rules if not
+                            any([ p in r for p in full_names ]) ]
+
+    return clean_rules
+
+
+def make_new_full_ruleset(current_rules, new_rules):
+    """
+    Prepends the specified rules in the given chains, if they exist.
+
+    If not, creates the chains.
+
+    Return a new, augmented list of rules, ready to replace the old rules.
+
+    """
+    # Start by creating the chains. They are all in the 'filter' section.  The
+    # only rules we need to define are the policy rules, which all end with a
+    # '_'. The other entries are for existing chains (such as
+    # 'ROMANA-T1S2-FORWARD'), so we don't need to define them again.
+    new_chains_to_declare = [ k for k in new_rules.keys() if k.endswith('_') ]
+    existing_chains = [ k for k in new_rules.keys() if not k.endswith('_') ]
+
+    rules = []
+    new_chains_declared = False
+    new_rules_defined   = False
+
+    for r in current_rules:
+        # The definition of the new chains happens first, towards the top of
+        # the rules in the '*filter' section.
+        if not new_chains_declared:
+            rules.append(r)
+            if r == "*filter":
+                for c in new_chains_to_declare:
+                    rules.append(":%s - [0:0]" % c)
+                new_chains_declared = True
+            continue
+
+        # Now continue and find the entries of those rules that
+        # existed already (such as 'ROMANA-T1S2-FORWARD').
+        for i, c in enumerate(existing_chains):
+            if r.startswith("-A %s" % c):
+                # This is a good place to define the rules for the new chains,
+                # if this wasn't done already.
+                if not new_rules_defined:
+                    for nc in new_chains_to_declare:
+                        # For each of the new chains we have a list of rules we
+                        # need to define.
+                        for nr in new_rules[nc]:
+                            rules.append(nr)
+                    new_rules_defined = True
+                # We pre-pend the rules we have for the existing rule.
+                for nr in new_rules[c]:
+                    rules.append(nr)
+                # We only need to perform any rule insertion at the first
+                # occurrence of the chain, so we keep track of whether we
+                # dealt with it already or not and skip if we have.
+                existing_chains.pop(i)  # We dealt with this chain already
+                break
+
+        rules.append(r)
+
+    return rules
+
+
+def apply_new_ruleset(rules):
+    """
+    Uses iptables-restore to apply a full, new ruleset.
+
+    """
+    p = subprocess.Popen(["iptables-restore"],
+                         stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    out, err = p.communicate('\n'.join(rules))
+    if err:
+        print "@@@ ERROR applying these rules..."
+        for i, r in enumerate(rules):
+            print "%3d: %s" % (i+1, r)
+        print
+        print "@@@ ERROR applying iptables: ", err
+        return False
+    else:
+        print "@@@ iptables rules successfully applied."
+        return True
+
+
+
+def policy_update(romana_address_scheme, policy_definition, delete_policy=False):
+    """
+    Using the romana address scheme and a policy definition as input,
+    create a new set of iptables rules and apply them.
+
+    NOTE! Since we do get/edit/write in separate steps, it would be possible
+    for someone else to clobber the rules before we have a chance to write
+    this. A lock of of some sort, or an otherwise atomic operation needs to be
+    implemented here. TODO!
+
+    """
+
+    # Create the new rules, based on the Romana addressing scheme and the
+    # policy definition.
+    new_rules = make_rules(romana_address_scheme, policy_definition)
+    """
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(new_rules)
+    """
+
+    # LOCK SECTION SHOULD START HERE...
+    # Get what iptables currently has
+    iptables_rules = get_current_iptables()
+
+    # Remove ALL rules relating in any way to a policy of the specified name.
+    clean_rules = \
+        delete_all_rules_for_policy(iptables_rules,
+                                    policy_definition['policy_name'],
+                                    policy_definition['owner_tenant_id'])
+
+    if delete_policy:
+        apply_new_ruleset(clean_rules)
+        return
+
+
+    # Create a new rule set that can be applied to iptables
+    rules = make_new_full_ruleset(clean_rules, new_rules)
+
+    apply_new_ruleset(rules)
+
+    # LOCK SECTION SHOULD END HERE...
+
+
+def parse_rule_specs(obj):
+    try:
+        rule = {}
+        rule["src_tenant"] = obj["object"]["metadata"]["labels"]["owner"]
+        rule["dst_tenant"] = rule["src_tenant"]
+        rule["dst_segment"] = obj["object"]["spec"]["podSelector"]["tier"]
+        rule["src_segment"] = obj["object"]["spec"]["allowIncoming"]["from"][0]["pods"]["tier"]
+        rule["port"] = obj["object"]["spec"]["allowIncoming"]["toPorts"][0]["port"]
+        rule["protocol"] = obj["object"]["spec"]["allowIncoming"]["toPorts"][0]["protocol"]
+        return rule
+    except Exception, e:
+        print "Cannot parse %s: %s" % (obj, e)
+        return None
+
+def get_tenants():
+    r = requests.get(tenant_url)
+    print 'Tenants service returned %s' % r.content
+    return simplejson.loads(r.content)
+
+def get_tenant_id_by_name(name, tenants):
+    for tenant in tenants:
+        if tenant['Name'] == name:
+            return tenant['Id']
+
+def get_segments(tenant_id):
+    r = requests.get(tenant_url + '/' + str(tenant_id) + '/segments')
+    return simplejson.loads(r.content)
+
+def get_segment_id_by_name(name, segments):
+    for segment in segments:
+        if segment['Name'] == name:
+            return segment['Seq']
+
+addr_scheme = {
+    "network_width" : 8,
+    "host_width" : 8,
+    "tenant_width" : 4,
+    "segment_width" : 4,
+    "endpoint_width" : 8,
+
+    "network_value" : 10,
+}
+
 
 def process(s):
     obj = simplejson.loads(s)
+    print obj
     op = obj["type"]
     details = obj["object"]["spec"]
-    if op == 'ADDED':
-        rule = parse_rule_specs(obj)
-        tenants = get_tenants()
-        print "Discovered tenants = %s" % tenants
-        tenant_id = get_tenant_id_by_name(rule['src_tenant'], tenants)
-        print "Discovered tenant_id = %s" % tenant_id
-        segments = get_segments(tenant_id)
-        print "Discovered segments = %s" % segments
-        src_segment_id = get_segment_id_by_name(rule['src_segment'], segments)
-        print "Discovered src_segment_id = %s" % src_segment_id
-        dst_segment_id = get_segment_id_by_name(rule['dst_segment'], segments)
-        print "Discovered dst_segment_id = %s" % dst_segment_id
-        #src_mask = makeU32rule(tenant_id, src_segment_id)
-        src_mask = _make_u32_match(tenant_id, src_segment_id, tenant_id, dst_segment_id)
-        print "Discovered src_mask = %s" % src_mask
-        #dst_mask = makeU32rule(tenant_id, dst_segment_id)
-        dst_mask = _make_u32_match(tenant_id, dst_segment_id, tenant_id, src_segment_id)
-        print "Discovered dst_mask = %s" % dst_mask
-        policy_name = obj["object"]["metadata"]["name"]
-        print "Discovered policy_name = %s" % policy_name
-        src_fw_name = make_forward_name(tenant_id, src_segment_id)
-        print "Discovered src_fw_name = %s" % src_fw_name
-        dst_fw_name = make_forward_name(tenant_id, dst_segment_id)
-        print "Discovered dst_fw_name = %s" % dst_fw_name
-        firewall_policy_name = make_policy_name(policy_name)
-        print "Discovered firewall_policy_name = %s" % firewall_policy_name
-        base_rules = make_base_rules(firewall_policy_name, src_mask, dst_mask)
-        print "Discovered base_rules = %s" % base_rules
-        in_rules = make_in_rules(firewall_policy_name, rule['port'])
-        print "Discovered in_rules = %s" % in_rules
-        out_rules = make_out_rules(firewall_policy_name, rule['port'])
-        print "Discovered out_rules = %s" % out_rules
-        install_chains(firewall_policy_name)
-        install_rules(base_rules)
-        install_rules(in_rules)
-        install_rules(out_rules)
-        install_first_jump(src_fw_name, firewall_policy_name)
-        install_first_jump(dst_fw_name, firewall_policy_name)
+    rule = parse_rule_specs(obj)
+    if not rule:
+        return
+    tenants = get_tenants()
+    print "Discovered tenants = %s" % tenants
+    tenant_id = get_tenant_id_by_name(rule['src_tenant'], tenants)
+    print "Discovered tenant_id = %s" % tenant_id
+    if not tenant_id:
+        print "Tenant %s not found" % rule['src_tenant']
+        return
+    segments = get_segments(tenant_id)
+    print "Discovered segments = %s" % segments
+    src_segment_id = get_segment_id_by_name(rule['src_segment'], segments)
+    print "Discovered src_segment_id = %s" % src_segment_id
+    dst_segment_id = get_segment_id_by_name(rule['dst_segment'], segments)
+    print "Discovered dst_segment_id = %s" % dst_segment_id
 
-        print "Added: %s" % rule
+    policy_definition = {                                                           
+        "policy_name" : obj['object']['metadata']['name'],
+        "owner_tenant_id" : tenant_id,
+        "target_segment_id" : dst_segment_id,
+        "allowFrom" : {                                                             
+            "segment_id" : src_segment_id,
+            "protocol" : "tcp",                                                     
+            "port" : obj['object']['spec']['allowIncoming']['toPorts'][0]['port']
+        }                                                                           
+    }                                                                               
+
+    if op == 'ADDED':
+        print "Adding policy: ", obj['object']['metadata']['name']
+        policy_update(addr_scheme, policy_definition)
     elif op == 'DELETED':
-        print "Deleted: %s" % details
+        print "Deleting policy: ", obj['object']['metadata']['name']
+        policy_update(addr_scheme, policy_definition, delete_policy=True)
     else:
         print "Unknown operation: %s" % op
 
