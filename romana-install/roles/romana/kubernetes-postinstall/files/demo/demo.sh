f5e868881ff5a9fded1388dc2a8822702bd7f311
Caleb Gilmour <cgilmour@gmail.com>
2016-02-26 13:02:03 +1300

    Include updates into demo script

--- romana-install/roles/romana/kubernetes-postinstall/files/demo/demo.sh
+++ romana-install/roles/romana/kubernetes-postinstall/files/demo/demo.sh
@@ -46,6 +46,10 @@ function relative() {
 function get_pods() {
     kubectl get pods -o json | jq '.items[] | { Name: .metadata.name, podIP: .status.podIP, NodeID: .spec.nodeName, Status: .status.phase }'
 }
+function get_pod_ip() {
+    kubectl get pod "$1" -o json | jq -r '.status.podIP'
+}
+
 SSH_NODE=$(kubectl get nodes | tail -1 | cut -f1 -d' ')
 trap "echo" EXIT
 cd "${0%/*}" # Change directory to where the script is
@@ -54,21 +58,31 @@ desc "Get a list of nodes in the environment"
 run "kubectl get nodes"
 desc "anybody here? let's see if we have a pod"
 run "get_pods"
-desc "How about we stand up a few pods with a resource controller"
+
+desc "How about we start up a few pods with a resource controller"
 run "kubectl create -f example-controller.yaml"
 desc "anybody here now?"
 run "get_pods"
+
 desc "create a pod on 'frontend' segment in 't1' tenant/owner"
 run "kubectl create -f pod-frontend.yaml"
+
 desc "create a pod on 'backend' segment in 't1' tenant/owner"
-run "kubectl create -f pod-backend.yaml"
+run "kubectl create -f pod-backend.yaml; sleep 5"
+
 desc "Let’s find out where the pods are"
 run "get_pods"
-desc "Log into nginx-frontend, check if we can reach the backend"
-run "kubectl exec nginx-frontend -t -i -- /bin/bash"
+
+desc "we should only see our 'internal' local interface"
+run "kubectl exec nginx-frontend -- ip addr"
+
+desc "Let's look at the state in the frontend segment"
+run "kubectl exec nginx-frontend -- curl $(get_pod_ip 'nginx-backend') --connect-timeout 5"
+
 desc "Now let's add a policy that permits frontend to connect to the backend"
-run "romana add-policy romana-np-frontend-to-backend.json"
-desc "Log into nginx-frontend, check if we can reach the backend"
-run "kubectl exec nginx-frontend -t -i -- /bin/bash"
+run "romana add-policy romana-np-frontend-to-backend.json; sleep 5"
+desc "Let's look at the state in the frontend segment"
+run "kubectl exec nginx-frontend -- curl $(get_pod_ip 'nginx-backend') --connect-timeout 5"
+
 desc "Demo completed (cleaning up)"
 run "romana remove-policy pol1; kubectl delete pod nginx-backend; kubectl delete pod nginx-frontend; kubectl delete replicationcontroller nginx-default"
