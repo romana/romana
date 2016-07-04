5f2d22186ff9f1cfea8f96dac046674cadaf792f
Caleb Gilmour <cgilmour@gmail.com>
2016-07-04 16:46:05 +1200

    Install CNI deps at earlier stage. Fix network policy

--- romana-install/roles/romana/postinstall/kubernetes/master/templates/demo.sh
+++ romana-install/roles/romana/postinstall/kubernetes/master/templates/demo.sh
@@ -101,10 +101,10 @@ desc "let's try to have the frontend load data from the backend"
 run "kubectl --namespace=tenant-a exec nginx-frontend -- curl $(get_pod_ip 'nginx-backend' 'tenant-a') --connect-timeout 5"
 
 desc "now let's add a policy that permits frontend to connect to the backend"
-run "curl -X POST -H 'Content-Type: application/yaml' -d @romana-np-frontend-to-backend.json http://{{ romana_master_ip }}:8080/apis/romana.io/demo/v1/namespaces/tenant-a/networkpolicys; sleep 5"
+run "curl -X POST -H 'Content-Type: application/yaml' -d @romana-np-frontend-to-backend.yml http://{{ romana_master_ip }}:8080/apis/romana.io/demo/v1/namespaces/tenant-a/networkpolicies; sleep 5"
 
 desc "this permits us to connect from frontend to backend"
 run "kubectl --namespace=tenant-a exec nginx-frontend -- curl $(get_pod_ip 'nginx-backend' 'tenant-a') --connect-timeout 5"
 
 desc "Demo completed (cleaning up)"
-run "curl -X DELETE http://{{ romana_master_ip }}:8080/apis/romana.io/demo/v1/namespaces/tenant-a/networkpolicys/pol1; kubectl --namespace=tenant-a delete pod nginx-backend; kubectl --namespace=tenant-a delete pod nginx-frontend; kubectl delete namespace tenant-a; kubectl delete replicationcontroller nginx-default; delete_tenant 'tenant-a'"
+run "curl -X DELETE http://{{ romana_master_ip }}:8080/apis/romana.io/demo/v1/namespaces/tenant-a/networkpolicies/pol1; kubectl --namespace=tenant-a delete pod nginx-backend; kubectl --namespace=tenant-a delete pod nginx-frontend; kubectl delete namespace tenant-a; kubectl delete replicationcontroller nginx-default; delete_tenant 'tenant-a'"
