9a82e30c57b7392adb9f6856dc54649496974077
pritesh <pritesh@users.noreply.github.com>
2016-09-20 10:31:04 -0700

    curl: turn on silient mode so disable verbose logging.

--- romana-install/roles/romana/install-agent/files/agent_proxy_delete_policy.sh
+++ romana-install/roles/romana/install-agent/files/agent_proxy_delete_policy.sh
@@ -4,5 +4,5 @@
 #
 body=`echo $1 | cut -d'=' -f2`
 echo Posting $body to http://localhost:9630/
-echo curl -H 'content-type: application/json' -d @- -X DELETE http://localhost:9630/
-echo $body | curl -H 'content-type: application/json' -d @- -X DELETE http://localhost:9630/
+echo curl -s -H 'content-type: application/json' -d @- -X DELETE http://localhost:9630/
+echo $body | curl -s -H 'content-type: application/json' -d @- -X DELETE http://localhost:9630/
