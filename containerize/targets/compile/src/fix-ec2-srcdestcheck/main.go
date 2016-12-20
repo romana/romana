9e8a68f8d57b1436e4eeb1bb692ab666ca8321e8
Caleb Gilmour <cgilmour@gmail.com>
2016-12-21 10:39:38 +1300

    Add copyright. Add to compile stage.

--- containerize/targets/compile/src/fix-ec2-srcdestcheck/main.go
+++ containerize/targets/compile/src/fix-ec2-srcdestcheck/main.go
@@ -1,3 +1,27 @@
+// Copyright (c) 2016 Pani Networks
+// All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// This package provides a proxy between kubernetes nodes using Romana CNI and
+// Romana services running on a kubernetes master.
+// When allocating an IP, the proxy looks up "labels" in the Kubernetes pod spec,
+// and adds that to the IPAM request.
+// When deallocating an IP, the proxy finds the IP previously allocated for the pod,
+// and passes that to IPAM. (The interface on the node is deleted before CNI is triggered,
+// so it can't be provided by the node directly.)
+// An endpoint for looking up kubernetes namespace information is also provided.
+
 package main
 
 import (
